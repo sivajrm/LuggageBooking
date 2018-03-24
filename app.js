364abb840f80f5efd45bc0da0a6811b3b57eee4f
Siva Sivasubramoniam Jayaram <sivajrm@gmail.com>
2018-03-24 13:45:38 -0700

    With express

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
+var express = require('express')
+var app = express()
 
-var http = require("http");
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-//Create HTTP server and listen on port 8000 for requests
-http.createServer(function (request, response) {
-
-   // Set the response HTTP header with HTTP status and Content type
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body "Hello World"
-   response.end('Hello World\n');
-}).listen(8000);
-
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000');
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
+})
