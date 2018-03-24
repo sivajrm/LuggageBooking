2214595313ef1b6385d057e44b2f8c37ccac60df
Siva Sivasubramoniam Jayaram <sivajrm@gmail.com>
2018-03-24 13:59:26 -0700

    with body-parser and router module

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 var express = require('express')
 var app = express()
 
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8000/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
